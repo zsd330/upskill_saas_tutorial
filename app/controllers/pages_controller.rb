f19ea65cd33ac51120f81d02f3c77f0589b5b1d8
Zachary Dulaney <zsd3@zips.uakron.edu>
2017-01-08 20:28:44 +0000

    Added contact form view, route, controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
     
     def about
     end
+    
+    def new
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "about", to: "pages#menu"
   get "Locations", to: "pages#Locations"
+  resources :contacts
 end
 
